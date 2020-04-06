c770f25c3520e2171bb641512d6175b9931258f0
Jozef Dochan <theglow666@gmail.com>
2020-04-06 18:28:07 +0200

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Script to start the backup script "bkup_rpimage.sh" via simple crontab entry# Target automounten lassen:
+
+BACKUPDIR=/mnt/Backup/
+BACKUPFILE=$(uname -n)-$(date +%F).img
+SHARE=//rpi.local/nas/Backup
+
+# create and mount destination folder
+mkdir -p $BACKUPDIR
+mount -t cifs -o user=,password= $SHARE $BACKUPDIR
+
+#cleanup apt cache
+sudo apt-get clean
+
+# Automounting target
+if [ -d "$BACKUPDIR" ]; then
+        # start script
+        bkup_rpimage.sh start -c $BACKUPDIR/$BACKUPFILE
+        pishrink.sh -z $BACKUPDIR/$BACKUPFILE
+fi
+
+umount $BACKUPDIR
